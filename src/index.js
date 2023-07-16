da2ff3f73f37a873e945f13da6af776c0cd96493
Aniket Agrawal <aniket.mpa@gmail.com>
2023-07-16 17:54:25 +0200

    Implement function to get moves from start to end
    The strategy is to keep track of the last position as
    parent until start position is reached. Start from end
    position and find a way to start position. Then keep
    accessing parent of the start position.

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import { createNode } from "../graphNode";
+
+function knightMovesTest(start, end) {
+    let vertDiff = [-2, 2, -1, 1];
+    let horzDiff = [-2, 2, -1, 1];
+    let visited = new Array(8);
+    for (let i = 0; i < 8; i++) {
+        visited[i] = new Array(8);
+        for (let j = 0; j < 8; j++) {
+            visited[i][j] = false;
+        }
+    }
+    let endNode = createNode(null, end, []);
+    let toVisit = [endNode];
+    
+    while (toVisit.length > 0) {
+        let nextToVisit = [];
+        for (let i = 0; i < toVisit.length; i++) {
+            let currentCoords = toVisit[i].getValue();
+            visited[currentCoords[0]][currentCoords[1]] = true;
+            for (let h = 0; h < 4; h++) {
+                let horzCoord = currentCoords[0] + horzDiff[h];
+                let validHorz = horzCoord >=0 && horzCoord < 8;
+                for (let v = 0; v < 4 && validHorz; v++) {
+                    if (Math.abs(horzDiff[h]) !== Math.abs(vertDiff[v])) {
+                        let vertCoord = currentCoords[1] + vertDiff[v];
+                        if (vertCoord >= 0 && vertCoord < 8) {
+                            let childNode = createNode(toVisit[i], [horzCoord, vertCoord], []);
+                            if (horzCoord === start[0] && vertCoord === start[1]) {
+                                return childNode;
+                            }
+                            if (!visited[horzCoord][vertCoord]) {
+                                toVisit[i].addChild(childNode);
+                                nextToVisit.push(childNode);
+                            }   
+                        }
+                    }
+                }
+            }
+        }
+        toVisit = nextToVisit;
+    }
+}
+
+function knightMoves(start, end) {
+    let current = knightMovesTest(start, end);
+    while (current !== null) {
+        console.log(current.getValue());
+        current = current.getParent();
+    }
+}
+
+knightMoves([0, 0], [1, 4]);
