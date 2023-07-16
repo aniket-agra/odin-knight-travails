520c392634533707d6080115745656362497402d
Aniket Agrawal <aniket.mpa@gmail.com>
2023-07-16 19:21:01 +0200

    Fix logic for updating start/end when clicking square

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ knightMoves([0, 0], [1, 4]);
 populateDisplay();
 let start = undefined, end = undefined;
 document.querySelectorAll(".chessboard > div").forEach(e => e.addEventListener("click", function (e) {
+    let clicked = e.target.classList[0];
+    let i = clicked[0] - 1, j = clicked[1] - 1;
     if (start === undefined) {
         start = [i - 1, j - 1];
     } else {
