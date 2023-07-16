06bc9d0554311231ba6b631e0df5fceb38e2c30f
Aniket Agrawal <aniket.mpa@gmail.com>
2023-07-16 15:41:12 +0200

    Add node factory

--- graphNode.js
+++ graphNode.js
@@ -1,3 +1,33 @@
-function createNode() {
+function createNode(parentNode = null, val = null, child = []) {
+    let parent = parentNode;
+    let value = val;
+    let children = child;
     
+    const addChild = function (graphNode) {
+        this.children.push(graphNode);
+    }
+
+    const getChildren = function () {
+        return this.children;
+    }
+
+    const setParent = function (graphNode) {
+        this.parent = graphNode;
+    }
+
+    const getParent = function () {
+        return this.parent;
+    }
+
+    const setValue = function (coords) {
+        this.value = coords;
+    }
+
+    const getValue = function () {
+        return this.value;
+    }
+
+    return {getValue, setValue, getParent, setParent, getChildren, addChild};
 }
+
+export {createNode};
