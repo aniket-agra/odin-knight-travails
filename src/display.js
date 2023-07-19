276bb5786c109f0aa1f873b4d3a99af4dcd6deed
Aniket Agrawal <agra.aniket@gmail.com>
2023-07-19 10:48:03 +0200

    Move chessboard styling to stylesheet

--- src/display.js
+++ src/display.js
@@ -3,12 +3,6 @@ function populateDisplay() {
 
     let chessBoard = document.createElement("div");
     chessBoard.classList.add("chessboard");
-    chessBoard.style.display = "grid";
-    chessBoard.style.gridTemplateRows = "repeat(8, 1fr)";
-    chessBoard.style.gridTemplateColumns = "repeat(8, 1fr)";
-    chessBoard.style.border = "2px solid black";
-    chessBoard.style.width = "40rem";
-    chessBoard.style.height = "40rem";
 
     for (let i = 1; i < 9; i++) {
         for (let j = 1; j < 9; j++) {
